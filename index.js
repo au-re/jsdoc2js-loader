c3e5bb85675fb0984d877acd242173b6155a9b68
aure <au.re@live.com>
2017-07-22 13:22:34 +0200

    Fix(package): Minor typos

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const jsdoc = require('jsdoc-api');
  * Loader function
  *
  * @param {string} source - string to parse by jsdoc-api
- * @return {array} - an array explaning the content of the source
+ * @return {array} - an array explaining the content of the source
  */
 module.exports = (source) => jsdoc.explainSync({ source });
-
